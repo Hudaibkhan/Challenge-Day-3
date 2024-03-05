9b348332557bc3cf3817bb0f979bef0b27753163
Hudaibkhan <161836104+Hudaibkhan@users.noreply.github.com>
2024-03-06 02:06:51 +0500

    Add files via upload
    I am student of Governor Sindh IT program

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+// Day-3-Challenge
+// QUESTION 7,8
+console.log(2 + 6); //addition
+console.log(14 - 6); //substraction
+console.log(4 * 2); //multiplication
+console.log(80 / 10); //division
+// QUESTION 9
+let favoriteNumber = 9;
+let message = "My Favorite Number is";
+console.log(`${message} ${favoriteNumber}`);
---
+++ app.ts
@@ -0,0 +1,37 @@
+// Day-3-Challenge
+
+// QUESTION 7,8
+console.log(2+6); //addition
+console.log(14-6); //substraction
+console.log(4*2); //multiplication
+console.log(80/10); //division
+
+// QUESTION 9
+let favoriteNumber: number= 9;
+let message: string="My Favorite Number is";
+console.log(`${message} ${favoriteNumber}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
